d294dc584199a7fce16b3085151a1e46dd2684c2
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-05-14 10:46:47 +0530

    welcome function

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+import chalkAnimation from 'chalk-animation';
+import chalk from 'chalk';
+import figlet from 'figlet';
+import input from 'input';
 var playerName;
 var score;
 
@@ -16,3 +20,24 @@ const questions = [
     new Question("Wanna type out this long text", 0.6),
     new Question("How about ranking up the difficulty", 1),
 ]
+
+async function welcome() {
+    figlet(
+        "Peek-n-Type",
+        { font: "Stick Letters" }, async (err, data) => {
+        if (err) {
+            console.log("Something went wrong");
+            console.dir(err);
+            return;
+        }
+
+        chalkAnimation.neon(data);
+
+        await sleep(2500);
+        console.log(chalk.bold("Welcome to the Peek-n-Type game"));
+        await sleep(2000);
+
+        await intro();
+    });
+}
+await welcome();
